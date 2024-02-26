757ed30246d2da3f295b2a665b63dcef58bc7013
l1f35uxx <alex908vk@gmail.com>
2024-02-26 23:50:40 +0300

    Initial commit (db_config + db_connect + gitignore)

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+  define('DB_SERVER', "");            // DB_SERVER
+  define('DB_NAME', "");              // DB_NAME
+  define('DB_LOGIN', "login");        // Login
+  define('DB_PASSWORD', "password");  // Passowrd
+?>
---
+++ db_connect.php
@@ -0,0 +1,25 @@
+<?php
+  class DB {
+    function __construct() {
+      $this->connect();
+    }
+
+    function __destruct() {
+      $this->close();
+    }
+
+    function connect() {
+      require 'db_config.php';
+      $con = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME) or die(mysql_error());
+      if (mysqli_connect_errno($con)) {
+        echo "Couldn't connect to DB server. Error: " . mysqli_connect_error();
+      }
+      return $con;
+    }
+
+    function close() {
+      $con = $this->connect();
+      mysqli_close($con);
+    }
+  }
+?>
